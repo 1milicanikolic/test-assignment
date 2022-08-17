da4d53fcc59c59540060cea8b339eaad149be814
1milicanikolic <nikolicmilica8@gmail.com>
2022-08-17 16:26:46 +0200

    commit

--- task1A/cypress/support/commands.js
+++ task1A/cypress/support/commands.js
@@ -2,11 +2,6 @@ import { productsPage } from "../page_objects/ProductsPage"
 import { loginPage } from "../page_objects/LoginPage"
 
 Cypress.Commands.add('buildForSecondTest', () => { 
-  //  cy.visit('/')
-   // loginPage.login(
-     //   Cypress.env('VALID_USER_NAME'),
-       // Cypress.env('VALID_USER_PASSWORD')
-    //) 
     productsPage.addBackpack.click()
     productsPage.addBikeLight.click()
 })
